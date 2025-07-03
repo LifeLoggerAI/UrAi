55206e3209b4d96c80de2b8225d804d8271007c0
Life Logger <lifeloggerai@gmail.com>
2025-07-03 20:52:58 +0000

    Next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@700&family=IBM+Plex+Sans:wght@400&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="hsl(260 43% 96%)" />
+        <meta name="theme-color" content="hsl(0 0% 100%)" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
