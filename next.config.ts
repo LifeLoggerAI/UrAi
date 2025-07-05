7b7c9cbf93dad04a5cf9a1f5015206745dac1bbc
Life Logger <lifeloggerai@gmail.com>
2025-07-05 02:56:24 +0000

    Make sure you fix all the problems this caused and causes

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,15 @@ import type { NextConfig } from 'next';
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com;
-    style-src 'self' 'unsafe-inline' https://accounts.google.com;
-    img-src 'self' data: https://placehold.co;
-    connect-src 'self' wss://*.googleapis.com https://*.googleapis.com https://accounts.google.com https://*.cloudworkstations.dev http://127.0.0.1:9099 ws://127.0.0.1:8080;
-    font-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com https://apis.google.com https://www.gstatic.com;
+    style-src 'self' 'unsafe-inline' https://accounts.google.com https://fonts.googleapis.com;
+    img-src 'self' data: https://placehold.co https://*.googleusercontent.com;
+    connect-src 'self' wss://*.googleapis.com https://*.googleapis.com https://accounts.google.com https://*.google.com https://*.cloudworkstations.dev http://127.0.0.1:9099 ws://127.0.0.1:8080;
+    font-src 'self' https://fonts.gstatic.com;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
+    frame-src 'self' https://accounts.google.com https://apis.google.com;
     frame-ancestors 'self' https://accounts.google.com;
     upgrade-insecure-requests;
 `.replace(/\s{2,}/g, ' ').trim();
@@ -28,6 +29,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
       },
