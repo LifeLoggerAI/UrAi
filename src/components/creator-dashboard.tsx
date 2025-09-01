e463c8df3050cba27eb7162206738f344ebada60
LifeLoggerAI <LifeLoggerAI@gmail.com>
2025-08-31 20:40:07 -0500

    Create creator dashboard component

---
+++ src/components/creator-dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreatorDashboard = () => {
+    return (
+        <div>
+            <h1>Creator Partnership Dashboard</h1>
+            {/* Add dashboard components here */}
+        </div>
+    );
+};
+
+export default CreatorDashboard;
