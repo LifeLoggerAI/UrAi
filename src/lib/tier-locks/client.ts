84bad5f36dcc166aef24cd3a1119c254c3268f31
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-08 20:59:39 -0500

    Export Tier 2 access hook and request helper

--- src/lib/tier-locks/client.ts
+++ src/lib/tier-locks/client.ts
@@ -1,5 +1,9 @@
 export { TIER2_FEATURES, TIER_ORDER, getTierRank, hasTierAtLeast } from './config';
 export { evaluateTierLock } from './evaluateTierLock';
+export { requestTier2AccessLock } from './requestTier2AccessLock';
+export { useTier2AccessLock } from './useTier2AccessLock';
+export type { RequestTier2AccessLockOptions, Tier2AccessLockResponse } from './requestTier2AccessLock';
+export type { UseTier2AccessLockOptions, UseTier2AccessLockResult } from './useTier2AccessLock';
 export type {
   ConsentSource,
   TierLockAuditEvent,
