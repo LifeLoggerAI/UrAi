6b69c39297fdd64dba7b94b45dbd87ca0c5fd809
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-08 20:35:02 -0500

    Add Tier 2 lock client exports

---
+++ src/lib/tier-locks/client.ts
@@ -0,0 +1,15 @@
+export { TIER2_FEATURES, TIER_ORDER, getTierRank, hasTierAtLeast } from './config';
+export { evaluateTierLock } from './evaluateTierLock';
+export type {
+  ConsentSource,
+  TierLockAuditEvent,
+  TierLockDecision,
+  TierLockEvaluationInput,
+  TierLockEvaluationResult,
+  TierLockFallback,
+  TierLockFeatureConfig,
+  TierLockFeatureId,
+  TierLockReason,
+  TierLockUserContext,
+  UraiTier,
+} from './types';
