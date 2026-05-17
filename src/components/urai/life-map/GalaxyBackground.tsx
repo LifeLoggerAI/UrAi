685ff63ade63a21d353b72da3e8efce68cba319e
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-17 05:41:02 -0500

    Add reusable GalaxyBackground component

---
+++ src/components/urai/life-map/GalaxyBackground.tsx
@@ -0,0 +1,8 @@
+export function GalaxyBackground() {
+  return (
+    <div className="urai-galaxy-bg" aria-hidden>
+      <div className="urai-galaxy-core" />
+      <div className="urai-galaxy-band" />
+    </div>
+  );
+}
