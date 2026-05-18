f7d44db52f972b96336454cc17788aa61f82a9b9
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-18 02:26:21 -0500

    Fix HomeScene type-only React import

--- src/components/urai/home/HomeScene.tsx
+++ src/components/urai/home/HomeScene.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect, useState, type FormEvent } from "react";
 import { cinematicEase } from "@/components/urai/motion/ascentMotion";
 import { useAscentTransition } from "@/components/urai/hooks/useAscentTransition";
 
