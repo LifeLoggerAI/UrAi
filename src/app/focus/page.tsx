b71af7e163a2fe1704124bfae992f4b460aa36cc
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-20 14:43:40 -0500

    Add route metadata to focus

--- src/app/focus/page.tsx
+++ src/app/focus/page.tsx
@@ -1,5 +1,9 @@
 import LifeMapUniverse from "@/components/life-map/LifeMapUniverse";
 
+export const metadata = {
+  title: "URAI Focus Chamber",
+};
+
 export default function FocusPage() {
   return <LifeMapUniverse initialView="focus" />;
 }
