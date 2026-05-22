aeed345c413479404353eef3e3ddbc9b0796a576
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-21 22:27:23 -0500

    Update root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,5 @@ import HomePage, { metadata } from "./home/page";
 export { metadata };
 
 export default function Page() {
-  return (
-    <>
-      <h1>Inner Sky Shrine</h1>
-      <h2>URAI</h2>
-      <HomePage />
-    </>
-  );
+  return <HomePage />;
 }
