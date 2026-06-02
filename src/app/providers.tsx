be71abb82653b1d32cc6a56c2962e8ac28821c80
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-02 02:08:36 -0500

    Wrap app with URAI voice provider and caption bubble

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,11 +3,16 @@
 import * as React from 'react';
 import { SpatialUniverseProvider } from '@/components/urai/SpatialUniverseProvider';
 import { UraiAudioProvider } from '@/providers/UraiAudioProvider';
+import { UraiVoiceProvider } from '@/providers/UraiVoiceProvider';
+import { UraiCaptionBubble } from '@/components/voice/UraiCaptionBubble';
 
 export default function AppProviders({ children }: { children?: React.ReactNode }) {
   return (
     <UraiAudioProvider>
-      <SpatialUniverseProvider>{children}</SpatialUniverseProvider>
+      <UraiVoiceProvider>
+        <SpatialUniverseProvider>{children}</SpatialUniverseProvider>
+        <UraiCaptionBubble />
+      </UraiVoiceProvider>
     </UraiAudioProvider>
   );
 }
