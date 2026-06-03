8e2e9020183db58bca317b1aacd18660813e78c0
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-02 20:45:01 -0500

    Add export and ritual providers to app root

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,10 +3,12 @@
 import * as React from 'react';
 import { SpatialUniverseProvider } from '@/components/urai/SpatialUniverseProvider';
 import { UraiAudioProvider } from '@/providers/UraiAudioProvider';
+import { UraiExportProvider } from '@/providers/UraiExportProvider';
 import { UraiGroundProvider } from '@/providers/UraiGroundProvider';
 import { UraiLegacyProvider } from '@/providers/UraiLegacyProvider';
 import { UraiLifeMapProvider } from '@/providers/UraiLifeMapProvider';
 import { UraiMirrorProvider } from '@/providers/UraiMirrorProvider';
+import { UraiRitualProvider } from '@/providers/UraiRitualProvider';
 import { UraiShadowProvider } from '@/providers/UraiShadowProvider';
 import { UraiVoiceProvider } from '@/providers/UraiVoiceProvider';
 import { UraiCaptionBubble } from '@/components/voice/UraiCaptionBubble';
@@ -15,17 +17,21 @@ export default function AppProviders({ children }: { children?: React.ReactNode 
   return (
     <UraiAudioProvider>
       <UraiVoiceProvider>
-        <UraiGroundProvider>
-          <UraiLifeMapProvider>
-            <UraiMirrorProvider>
-              <UraiShadowProvider>
-                <UraiLegacyProvider>
-                  <SpatialUniverseProvider>{children}</SpatialUniverseProvider>
-                </UraiLegacyProvider>
-              </UraiShadowProvider>
-            </UraiMirrorProvider>
-          </UraiLifeMapProvider>
-        </UraiGroundProvider>
+        <UraiExportProvider>
+          <UraiRitualProvider>
+            <UraiGroundProvider>
+              <UraiLifeMapProvider>
+                <UraiMirrorProvider>
+                  <UraiShadowProvider>
+                    <UraiLegacyProvider>
+                      <SpatialUniverseProvider>{children}</SpatialUniverseProvider>
+                    </UraiLegacyProvider>
+                  </UraiShadowProvider>
+                </UraiMirrorProvider>
+              </UraiLifeMapProvider>
+            </UraiGroundProvider>
+          </UraiRitualProvider>
+        </UraiExportProvider>
         <UraiCaptionBubble />
       </UraiVoiceProvider>
     </UraiAudioProvider>
