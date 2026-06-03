60f7943287a2be27e414b5aa5b8981fb3027ffac
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-03 13:14:56 -0500

    Extend Jest setup for URAI QA harness

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -12,6 +12,11 @@ Object.defineProperty(window, 'matchMedia', {
   value: (query: string) => ({ matches: false, media: query, onchange: null, addListener: jest.fn(), removeListener: jest.fn(), addEventListener: jest.fn(), removeEventListener: jest.fn(), dispatchEvent: jest.fn() }),
 });
 
+window.HTMLMediaElement.prototype.play = jest.fn().mockResolvedValue(undefined);
+window.HTMLMediaElement.prototype.pause = jest.fn();
+Object.defineProperty(navigator, 'vibrate', { writable: true, value: jest.fn() });
+
 beforeEach(() => {
   window.localStorage.clear();
+  jest.clearAllMocks();
 });
