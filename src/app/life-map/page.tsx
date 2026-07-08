896f7e90ee52e5bf1f170a6926b3fe0fd8c3f857
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-07 21:18:41 -0500

    Surface asset factory slots on Life Map

--- src/app/life-map/page.tsx
+++ src/app/life-map/page.tsx
@@ -1,5 +1,11 @@
+import { AssetFactoryRoutePanel } from "@/components/urai/assets/AssetFactoryRoutePanel";
 import SpatialLifeMap from "@/components/spatial-life-map/SpatialLifeMap";
 
 export default function LifeMapPage() {
-  return <SpatialLifeMap />;
+  return (
+    <>
+      <SpatialLifeMap />
+      <AssetFactoryRoutePanel route="/life-map" title="Life Map Asset Factory" />
+    </>
+  );
 }
